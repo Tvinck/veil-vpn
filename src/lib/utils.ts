daf0ec0fcb278a512636faca6ea5f0b067314881
Tvinck <112720884+Tvinck@users.noreply.github.com>
2026-06-12 23:06:33 +0300

    chore: configure new design packages and utils helper

---
+++ src/lib/utils.ts
@@ -0,0 +1,9 @@
+import { type ClassValue, clsx } from 'clsx'
+import { twMerge } from 'tailwind-merge'
+
+/**
+ * Merges Tailwind classes, resolving conflicts correctly.
+ */
+export function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs))
+}
